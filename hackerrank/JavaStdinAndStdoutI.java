e2cafe293594f4fecd44d641096cba37b235bd1b
Muhammad Rohman Irfanuddin <muhammad.rohman.irfanuddin09@gmail.com>
2018-07-16 16:43:22 +0700

    02. Java Stdin and Stdout I

---
+++ hackerrank/JavaStdinAndStdoutI.java
@@ -0,0 +1,19 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+public class JavaStdinAndStdoutI {
+
+	public static void main(String[] args) {
+		Scanner cin = new Scanner(System.in);
+		int a = cin.nextInt();
+		int b = cin.nextInt();
+		int c = cin.nextInt();
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		
+	}
+
+}
