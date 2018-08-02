740dd55968a523d3547a8f455dd7d8893a1f417e
Muhammad Rohman Irfanuddin <muhammad.rohman.irfanuddin09@gmail.com>
2018-08-02 13:56:10 +0700

    05. Java Output Formatting

---
+++ hackerrank/JavaOutputFormatting.java
@@ -0,0 +1,25 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+public class JavaOutputFormatting {
+
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            /**
+             * if you want input unlimited value, use: sc.hasNext();
+             * */
+            for(int i=0;i<3;i++)
+            {
+                String s1=sc.next();
+                int x=sc.nextInt();
+                System.out.printf("%-15s%03d\n", s1, x);
+            }
+            System.out.println("================================");
+
+    }
+}
+
+
+
